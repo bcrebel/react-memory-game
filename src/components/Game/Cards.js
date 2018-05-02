7cb6c0245e1c2e733abb3f856db8120e1f204fc3
Carron White <carronwhite@gmail.com>
2018-05-02 13:42:08 -0400

    Basic functionality

--- src/components/Game/Cards.js
+++ src/components/Game/Cards.js
@@ -1,30 +1,73 @@
 import React from 'react'
 import { levels } from './Levels'
+import styles from './Game.scss'
 
-export const formatCards = level => {
-  let symbols;
-  if(level === 'beginner') {
-    symbols = levels[0].cards
-  } else if( level === 'hard') {
-    symbols = levels[1].cards
-  }
+class Card extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			flipped: false,
+			queue: []
+		}
 
-  return symbols.map((symbol, idx) => 
-    <li key={idx}>{symbol}</li>
-  )
+		this.toggleClass = this.toggleClass.bind(this)
+	}
+
+	toggleClass() {
+		const currentState = this.state.flipped;
+		this.setState({ flipped: !currentState })
+	};
+
+	render() {  
+
+		return (<li className={this.state.flipped ? styles['flipped'] : null} 
+			onClick={this.toggleClass}> 
+			{this.props.children}
+		</li>)
+	}
 }
 
 class CardContainer extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      level: 'beginner',
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			level: 'hard',
+			queue: []
+		}
+		this.addToQueue = this.addToQueue.bind(this)
+	}
 
-  render() {
-    return <ul>{formatCards(this.state.level)}</ul>
-  }
+	addToQueue(symbol) {
+		this.state.queue.push(symbol)
+		console.log(this.state.queue)
+	}
+
+	render() {  
+		const formatCards = level => {
+			let symbols;
+			if(level === 'easy') {
+				this.state.level = 'easy'
+				symbols = levels[0].cards
+			} else if(level === 'hard') {
+				this.state.level = 'hard'
+				symbols = levels[1].cards
+			}
+
+			return symbols.map((symbol, idx) => 
+
+				<Card key={idx} id={symbol}>
+					<div>
+						<figure className={styles.front}></figure>
+						<figure className={styles.back}>{symbol}</figure>
+					</div>
+				</Card>
+			)
+		}
+
+		return <ul className={styles[this.state.level]}>
+			{formatCards(this.state.level)}
+		</ul>
+	}
 }
 
 export default CardContainer
